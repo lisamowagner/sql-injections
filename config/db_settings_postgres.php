5858ac19402168c603ba9bad44f521e55434fb46
Lisa <lisamowagner@gmail.com>
2022-06-13 21:16:18 +0200

    Updated settings

--- config/db_settings_postgres.php
+++ config/db_settings_postgres.php
@@ -2,7 +2,7 @@
     //Customize Settings below to connect to your database
     $dbuser = 'admin'; //PostgreSQL database username
     $dbpass = 'admin'; //PostgreSQL database password
-    $dbname = 'bricks';  //PostgreSQL database name
+    $dbname = 'HH11';  //PostgreSQL database name
     $host = '192.168.96.58';  //PostgreSQL database host
     $showhint = true;  //Shows the last executed SQL query
     $server = 'http://127.0.0.1';  //The protocol and server name to use in fully-qualified URLs
