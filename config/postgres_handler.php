1b74c0ba12ec69ef6e97855cd58f4d87a170fbcb
Lisa Mo Wagner <hey@lisamowagner.com>
2022-06-17 00:00:11 +0200

    db connection via env var

--- config/postgres_handler.php
+++ config/postgres_handler.php
@@ -1,7 +1,7 @@
 <?php
-    require_once(dirname(dirname(__FILE__)) . '/config/db_settings_postgres.php');
+/*     require_once(dirname(dirname(__FILE__)) . '/config/db_settings_postgres.php');
     ini_set('display_errors',1); 
     error_reporting(E_ALL);
-    $con = pg_connect("host=$host dbname = $dbname user=$dbuser password=$dbpass");
-    // $conn = pg_connect(getenv("DATABASE_URL")); connect to Heroku PostgreSQL Database
+    $con = pg_connect("host=$host dbname = $dbname user=$dbuser password=$dbpass"); */
+    $conn = pg_connect(getenv("DATABASE_URL"));
 ?>
